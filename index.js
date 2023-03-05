c854d917944a50a0766878551c7822a67969775d
David Moulton <dbmoulton@gmail.com>
2023-03-04 21:15:34 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+// function findMatching(collection, string) {
+//   for (const drivers of collection) {
+//     if (drivers.name === string) {
+//       return (drivers.name);
+//     }
+//   }
+// }
+
+// function fuzzyMatch(array, string){
+
+// }
+
+function findMatching(array, string) {
+   return array.filter((name) => name.toLowerCase() === string.toLowerCase())
+}
+function fuzzyMatch(array, string) {
+  return array.filter((name) => name.startsWith(string))
+ }
+ function matchName(array, string) {
+  return array.filter((obj) => obj.name == string)
+}
